16932ed4171be40a51dff22dc540b1359b1c7f58
david <david.svejda2000@gmail.com>
2022-10-12 13:19:47 +0200

    Made first version of gradient descent, and made tests for it

---
+++ solvers.py
@@ -0,0 +1,47 @@
+import numpy as np
+from warnings import warn
+
+
+
+def convergence_warning(grad, eta):
+    """Issue a convergence warning
+
+    Args:
+        grad (float): Gradient that was achieved.
+        eta (float): Convergence cut off.
+    """    
+    warn(f"Gradient descent didn't converge, stepsize was {grad} , limit is set to {eta}.", stacklevel=2)
+
+def gradient_descent(derivative, beta0, kwargs, lr, eta = 1e-8, max_iter = 50, momentum = 1, log = False):
+    """Perform gradient descent to optimise beta according to the derivatives of the cost function as defined by derivative
+
+    Args:
+        derivative (func(beta, **kwargs)): derivative of beta.
+        beta0 (np.arraylike): initial value of the parameters to be optimised.
+        kwargs (dict): kwargs for derivative function, such as data.
+        lr (float): learning rate.
+        eta (float, optional): Max error. Defaults to 1e-8.
+        max_iter (int, optional): Max number of steps. Defaults to 50.
+        momentum (float, optional): Size of the momentum effect. Defaults to 1.
+        log (Bool, optional): Set to True to keep track of a log of the beta values. Defaults to False.
+    """    
+    
+    beta = np.array(beta0)
+    cary = 1 - 1./momentum
+    d_beta = 0
+    if log: log_list = [beta.copy()]
+    iters = 0
+    while True:
+        d_beta = lr * derivative(beta, **kwargs) + cary * d_beta
+        beta -= d_beta
+        iters += 1
+        if log: log_list.append(beta.copy())
+        if (np.linalg.norm(d_beta) < eta): break
+        if (iters >= max_iter):
+            convergence_warning(np.linalg.norm(d_beta), eta)
+            break
+            
+    # print(beta, iters)
+    if log: return beta, iters, log_list
+    return beta, iters
+    
---
+++ tests.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pytest
+import warnings
+import matplotlib.pyplot as plt
+
+
+
+
+def test_gradient_descent():
+    from solvers import gradient_descent
+    
+    """
+    C(beta) = 3 (beta0 - 2)**2 + (beta1 - 3)**2
+    beta should converge to (2, 3)
+    """
+    def derivative(beta):
+        return np.array((6*(beta[0]-2), 2*(beta[1]-3)))
+    
+    beta0 = np.array((0.,0.))
+    
+    
+    #################### BASIC ######################
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter("always")
+        # Cause all warnings to always be triggered.
+        
+        
+        beta, iters = gradient_descent(derivative, beta0, {}, 1)
+        assert len(w) == 1
+        assert "converge" in str(w[-1].message)
+        beta, iters = gradient_descent(derivative, beta0, {}, 0.2)
+        assert len(w) == 1
+        assert np.allclose(beta, np.array((2., 3.)), 1e-5, 1e-5)
+        beta, iters = gradient_descent(derivative, beta0, {}, 0.1)
+        assert len(w) == 2
+        assert "converge" in str(w[-1].message)
+        beta, iters = gradient_descent(derivative, beta0, {}, 0.1, max_iter=100)
+        assert len(w) == 2
+        assert np.allclose(beta, np.array((2., 3.)), 1e-5, 1e-5)
+    
+    
+    #################### MOMENTUM ######################
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter("always")
+        # Cause all warnings to always be triggered.
+    
+        beta, iters, log = gradient_descent(derivative, beta0, {}, 0.08, momentum = 1.8, log = True)
+        assert len(w) == 0
+        # print(beta, iters)
+        # log = np.array(log)
+        # plt.plot(log[:,0], log[:,1])
+        # plt.savefig("test.pdf")
+
+
+if __name__ == "__main__":
+    test_gradient_descent()
