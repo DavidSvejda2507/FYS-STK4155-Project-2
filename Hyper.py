a99ecf7c71e0489d7c9d75fb63221410d7fab992
henrik breitenstein <henrik.breitenstein@bremo.org>
2022-11-11 17:06:49 +0100

    Changed data imported dims and made hyper.py use fixed lambda -> fixed lr, epoch -> fixed lambda instead

--- Hyper.py
+++ Hyper.py
@@ -3,58 +3,105 @@ import matplotlib.pyplot as plt
 import NeuralNet as NN
 import optimisers as op
 import ActivationFunctions as AF
+import Data
+
+ln10 = np.log(10)
 
 def func(x, y):
     return np.exp(x*x+y*y)
 
-def costFunc(pred, correct):
-    diff = (pred-correct)
+def Accuracy(predictions, targets):
+    numbers = np.arange(0, 10, 1)
+    predicted_values = numbers[np.argmax(predictions, axis=1)]
+    diff = np.equal(predicted_values, targets)
     return diff**2, diff*2
 
-def lr_ep_error(n_epochs, inputs, targets, costFunc, model):
+def Cross_Entropy(predictions, targets):
+    predictions = predictions.T
+    predicted_values = np.reshape(np.take_along_axis(predictions, np.expand_dims(targets, axis=1), axis=1), (len(predictions), ))
+    error = -np.log10(predicted_values)
+    derror = -1/(predicted_values*ln10)
+    return error, derror
+
+def lr_ep_error(n_epochs, inputs, targets, test_data, test_targets, costFunc, model):
     epochs = 0
     while epochs <= n_epochs:
         model.back_propagate(inputs, targets, costFunc)
         epochs += 1
-    predictions = model.feed_forward(inputs)
-    error = costFunc(predictions, targets)[0].mean()
-    return error
+    predictions = model.feed_forward(test_data)
+    predictions = predictions/np.amax(predictions, axis=0)
+    Acc = Accuracy(predictions.T, test_targets)[0].mean()
+    Cross_Ent = Cross_Entropy(predictions, test_targets)[0].mean()
+    return Acc, Cross_Ent
 
-def FindOpt(l_range, lr_range, ep_range, costFunc, af, opt):
+def FixedLambda(L, lr_range, ep_range, data, targets, test_data, test_targets, costFunc, shapes, af, opt):
+    min = 1e8
+    for k, lr in enumerate(lr_range):
+        print('.', end='', flush=True)
+        for j, n_epoch in enumerate(ep_range):
+            print(',', end='', flush=True)
+            model = NN.Model(shapes, [af]*2, opt(lr, L))
+
+            inputs = data
+
+            Acc, Cross_Ent = lr_ep_error(n_epoch, inputs, targets, test_data, test_targets, costFunc, model)
+
+            if Cross_Ent<min:
+                min = Cross_Ent
+                final_acc = Acc
+                k_min = k
+                j_min = j
+    #k = lr, j = epochs
+    return k_min, j_min
+
+def FixedLrEpoch(lr, n_epoch, data, targets, test_data, test_targets, costFunc, shapes, af, opt):
     min = 1e8
     for i, L in enumerate(l_range):
-        print('\n')
-        print('Completion:', i/len(l_range))
-        for lr in lr_range:
-            print('.', end='', flush=True)
-            for n_epoch in ep_range:
-                print(',', end='', flush=True)
-                model = NN.Model(shapes, [af]*2, opt(lr, L))
+        model = NN.Model(shapes, [af]*2, opt(lr, L))
 
-                inputs = np.random.rand(2,200)
-                targets = func(inputs[0], inputs[1])
+        inputs = data
 
-                error = lr_ep_error(n_epoch, inputs, targets, costFunc, model)
+        Acc, Cross_Ent = lr_ep_error(n_epoch, inputs, targets, test_data, test_targets, costFunc, model)
 
-                if error<min:
-                    min = error
-                    ep_min = n_epoch
-                    lr_min = lr
-                    L_min = L
-        print('\n')
-    return L_min, lr_min, ep_min, min
+        if Cross_Ent<min:
+            min = Cross_Ent
+            final_acc = Acc
+            i_min = i
 
-def FindPlateau(cutoff, l_range, lr_range, ep_range, costFunc, af, opt):
+    return i_min
+
+def FindOpt(data, targets, test_data, test_targets, l_range, lr_range, ep_range, costFunc, af, opt):
+    min = 1e8
+    print('\n')
+    print('Fixed Lambda 1:')
+    L = l_range[int(len(l_range)/2)]
+    k_min, j_min = FixedLambda(L, lr_range, ep_range, data, targets, test_data, test_targets, costFunc, shapes, af, opt)
+    i_min = FixedLrEpoch(lr, n_epoch, l_range, data, targets, test_data, test_targets, costFunc, shapes, af, opt)
+    k_min, j_min = FixedLambda(l_range[i_min], lr_range, ep_range, targets, test_data, test_targets, costFunc, shapes, af, opt)
+    return i_min, k_min, j_min, min, final_acc
+
+def FindPlateau(data, targets, test_data, test_targets, cutoff, l_range, lr_range, ep_range, costFunc, af, opt):
     dE = 1e8
-    L_min, lr_min, ep_min, min = FindOpt(l_range, lr_range, ep_range, costFunc, af, opt)
+    # i = L, k = lr, j = epoch
+    i_min, k_min, j_min, min, final_acc = FindOpt(data, targets, test_data, test_targets, l_range, lr_range, ep_range, costFunc, af, opt)
     while dE > cutoff:
-            l_range = np.linspace(0.8*L_min, 1.2*L_min, len(l_range))
-            lr_range = np.linspace(0.8*lr_min, 1.2*lr_min, len(lr_range))
-            ep_range = np.linspace(0.8*ep_range, 1.2*ep_range, len(ep_range))
-            L_min, lr_min, ep_min, min_new = FindOpt(l_range, lr_range, ep_range, costFunc, af, opt)
+            NewRun = 'New Run'
+            print(f'{NewRun:-^20}')
+            l_range = np.linspace(l_range[i_min-1], l_range[i_min+1], len(l_range))
+            lr_range = np.linspace(lr_range[k_min-1], lr_range[k_min+1], len(lr_range))
+            ep_range = np.linspace(ep_range[j_min-1], ep_range[j_min+1], len(ep_range))
+            i_min, k_min, j_min, min_new, acc_new = FindOpt(l_range, lr_range, ep_range, costFunc, af, opt)
             dE = abs(min-min_new)
             min = min_new
+
+    # Found minimum, extracting values
+    L_min = l_range[i_min]
+    lr_min = lr_range[k_min]
+    ep_min = ep_range[j_min]
+
     return L_min, lr_min, ep_min
+
 [l_range, lr_range, ep_range] = [np.linspace(1, 10, 10), np.linspace(0, 10, 10), np.logspace(1, 3, 10)]
-shapes = (2, 20, 1)
-print(FindOpt(l_range, lr_range, ep_range, costFunc, AF.ReLU(), op.MomentumOptimiser))
+shapes = (64, 20, 10)
+train, test, val, train_tar, test_tar, val_tar = Data.load_data()
+print(FindOpt(train, train_tar, test, test_tar, l_range, lr_range, ep_range, Cross_Entropy, AF.ReLU(), op.MomentumOptimiser))
--- NeuralNet.py
+++ NeuralNet.py
@@ -3,9 +3,9 @@ import numpy as np
 
 
 class Model():
-    
+
     def __init__(self, shapes, AFs, optimiser):
-        """Make a neural network with randomised initial 
+        """Make a neural network with randomised initial
 
         Args:
             shapes ([int]): Shapes of the input, each intermediate layer, and the output
@@ -18,7 +18,7 @@ class Model():
             self.layers.append(Layer(shapes[i], shapes[i+1], AFs[i]))
         self.optimiser = optimiser
         self.optimiser.set_model(self)
-    
+
     def feed_forward(self, in_data):
         """Feed the input through the network
 
@@ -27,11 +27,11 @@ class Model():
 
         Returns:
             np.array: Output data
-        """        
+        """
         for layer in self.layers:
             in_data = layer.feed_forward(in_data)
         return in_data
-    
+
     def back_propagate(self, in_data, target, costFunc):
         _, derivatives, Cost = self.layers[0].back_propagation(in_data, self.layers[1:], target, costFunc)
         derivatives.reverse()
@@ -39,7 +39,7 @@ class Model():
         return Cost.mean()
 
 class Layer():
-    
+
     def __init__(self, in_data, output, AF):
         """Make a Neural Network layer
 
@@ -47,24 +47,24 @@ class Layer():
             in_data (int): size of the input
             output (int): size of the layer
             AF ((func(np.array -> np.array)), derivative): Activation function
-        """        
+        """
         self.W = np.random.rand(output, in_data)
         self.B = np.random.rand(output, 1)
         self.AF = AF[0]
         self.dF = AF[1]
-        
+
     def feed_forward(self, in_data):
         """Feed the input through the layer
 
         Args:
             in_data (np.array): Input data with shape (size of one input, number of inputs)
-        
+
         Returns:
             np.array: Output data
         """
         in_data = (self.W @ in_data) + self.B
         return self.AF(in_data)
-    
+
     def back_propagation(self, in_data, layers, target, costFunc):
         """Feed the input forward and then propagate the derivative backward
 
@@ -78,16 +78,17 @@ class Layer():
             dCdIN: Derivatives of the cost function with respect to each of the inputs of the layer
             list_: List of the derivatives of all of the variables in this layer and the following layers
             Cost: Value of the cost function
-        """   
+        """
+
         intermediate_sum = (self.W @ in_data) + self.B
         output = self.AF(intermediate_sum)
-        
+
         if len(layers) > 0:
             dCdOUT, list_, Cost = layers[0].back_propagation(output, layers[1:], target, costFunc)
         else:
             list_ = []
             Cost, dCdOUT = costFunc(output, target)
-        
+
         dCdB = self.dF(intermediate_sum, output)*dCdOUT
         dCdW = in_data[np.newaxis,:,:] * dCdB[:, np.newaxis, :]
         dCdIN = np.tensordot(self.W, dCdB, ((0,), (0,)))
