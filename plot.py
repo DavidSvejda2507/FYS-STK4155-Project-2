debd14a5a36ba8af410226c0eacd6ae6ae884c87
david <david.svejda2000@gmail.com>
2022-11-12 21:14:40 +0100

    Plotting lr-ep works now

--- plot.py
+++ plot.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-figsize = (3.313, 2)
+figsize = (3.313, 3)
 
 
 def plot_lr_epochs(filename: str, fig_name: str, fig_title: str):
@@ -10,14 +10,18 @@ def plot_lr_epochs(filename: str, fig_name: str, fig_title: str):
     plt.figure(figsize=figsize)
     plt.imshow(data[1:,1:])
     
-    plt.xticks(data[1:, 0])
-    plt.yticks(data[0, 1:])
-    plt.xscale("log")
-    plt.yscale("log")
+    plt.yticks(ticks = range(5), labels = data[1:, 0])
+    plt.xticks(ticks = range(5), labels = map((int), data[0, 1:]))
+    # plt.xscale("log")
+    # plt.yscale("log")
+    # plt.ylim(1e-4, 1e2)
+    # plt.xlim(1e-4, 1e2)
     plt.xlabel("epochs")
     plt.ylabel("learning rate")
     
+    plt.colorbar()
     plt.title(fig_title)
+    plt.tight_layout()
     plt.savefig(fig_name)
     plt.show()
     
@@ -42,4 +46,4 @@ def plot_lamda(filename: str, fig_name: str, fig_title: str):
     fig.show()
     
 if __name__ == "__main__":
-    plot_lr_epochs("Data/NrHidden0/MomentumOptimiser/LrEpoch/Acc_L_5.txt", "test.pdf", "title")
+    plot_lr_epochs("Data/NrHidden0/MomentumOptimiser/LrEpoch/Ent_L_5.npy", "test.png", "title")
