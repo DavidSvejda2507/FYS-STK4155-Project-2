79836219293d36eb4e18e79f5ceff5f8b2566a07
giammy00 <puleogianm@gmail.com>
2022-11-15 11:20:59 +0100

    removed useless lambda

--- optimisers.py
+++ optimisers.py
@@ -8,9 +8,8 @@ class Optimiser():
     Any other optimiser should be subclass of this one, such that set_model(self, model) and update(self, derivatives) are callable
     """    
     
-    def __init__(self, lr, lamda = 0) -> None:
+    def __init__(self, lr) -> None:
         self.lr = lr
-        #self.lamda = lamda
         self.model = None
     
     def set_model(self, model) -> None:
@@ -45,20 +44,21 @@ class Optimiser():
         input: derivatives is a list of lists, each of the kind [ dCdW , dCdB]
                 dCdW, dCdB are np.array (or np.ndarray?)
         '''
+
         dbetas = []
         for derivative in derivatives:
             dbetas.append([  self.lr*derivative[0].mean(axis=2), self.lr*derivative[1].mean(axis=1)[:,np.newaxis]  ]  )
-        
+
         return dbetas
 
 
 
 class MomentumOptimiser(Optimiser):
     
-    def __init__(self, lr, momentum = 1, lamda = 0) -> None:
+    def __init__(self, lr, momentum = 1) -> None:
         self.carry = 1. - 1./momentum
         self.velocity = None
-        super().__init__(lr, lamda)
+        super().__init__(lr)
 
     def set_model(self, model):
         super().set_model(model)
@@ -66,24 +66,6 @@ class MomentumOptimiser(Optimiser):
         #no need for if test in long loops (gradient descent)
         self.velocity = [ [ np.zeros(shape=layer.W.shape), np.zeros(shape=layer.B.shape)] for layer in self.model.layers ]
 
-    ##note by gianmarco: commenting out old update version, now the update function changed its name
-    ##to "update_model" and does not need to be
-    ##over ridden, the only thing which needs 
-
-    # def update(self, derivatives):
-    #     #at the first step, simply compute gradient descent step using plain gradient.
-    #     if self.velocity is None:
-    #         self.velocity = derivatives
-
-    #     #at every step use momentum based gd to compute gd step.
-    #     else:#####PROBLEM: THIS ELSE DOES NOT REALLY UPDATE ANYTHING(!?)
-    #         for vel, der in zip(self.velocity, derivatives):
-    #             
-
-        
-    #     #doubt about the following line: self.velocity at this point should be still be velocity before the else statement
-    #     super().update(self.velocity)  
-    # 
     def compute_dbetas(self, derivatives):
         self.velocity = [ [self.lr*(self.carry * vel[0] + der[0].mean(axis = 2)),
                            self.lr*(self.carry * vel[1] + der[1].mean(axis = 1)[:,np.newaxis])]
@@ -112,8 +94,8 @@ class AdaGradOptimiser( Optimiser ):
         #update G= sum of gradients squared
         self.update_G(derivatives)
         #first compute dbeta (at first step, self.G=0, so only need to update it after computaiton of dbeta)
-        dbeta = [[der[0].mean(axis = 2)              *self.lr/ (np.sqrt(Gsum[0])),
-                  der[1].mean(axis = 1)[:,np.newaxis]*self.lr/ (np.sqrt(Gsum[1])) ]
+        dbeta = [[der[0].mean(axis = 2)              *self.lr/ (np.sqrt(Gsum[0]+self.epsilon)),
+                  der[1].mean(axis = 1)[:,np.newaxis]*self.lr/ (np.sqrt(Gsum[1]+self.epsilon)) ]
         for Gsum, der, layer in zip(self.G, derivatives, self.model.layers)]
         return dbeta
 
@@ -126,7 +108,7 @@ class AdaGradOptimiser( Optimiser ):
         return
 
 class RMSPropOptimiser( AdaGradOptimiser ):
-    #subclass of AdaGradOptimiser, cause compute_dbetas is the same.
+    #subclass of AdaGradOptimiser, because compute_dbetas is the same.
 
     def __init__(self, lr, epsilon=1e-8, gamma=0.9, lamda=0):
 
