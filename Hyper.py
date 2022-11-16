352f5ab7b4d9533046df1599d5b007fd5960e8ca
david <david.svejda2000@gmail.com>
2022-11-16 18:14:46 +0100

    Improvements to Hyper.py, and writing code blocks for single hidden layer

--- Hyper.py
+++ Hyper.py
@@ -33,15 +33,11 @@ def Cross_Entropy(predictions, targets):
 
     Returns: (n_images, ) with a values for how certain it is, (n_images, ) with derivative
     """
-    # print(predictions)
     predictions = predictions.T
     expanded = np.expand_dims(targets, axis=1)
     predicted_values = np.reshape(np.take_along_axis(predictions, expanded, axis=1), (len(predictions), ))
-    # print(predicted_values)
     error = -np.log10(abs(predicted_values)+1e-8)
-    # print(error)
     derror = -1/(abs(predicted_values*ln10)+1e-8)
-    # print(derror)
     zeros = np.zeros((10, len(predicted_values)))
     i = 0
     for k, j in zip(derror, targets):
@@ -75,23 +71,34 @@ def FixedLambda(L, lr_range, ep_range, nr_batches, data, targets, test_data, tes
     lr_ep_Acc[1:, 0] = lr_range
     lr_ep_Acc[0, 1:] = ep_range
 
+    batches = np.array_split(data, nr_batches, axis=1)
+    batches_targets = np.array_split(targets, nr_batches, axis=0)
+    
     for k, lr in enumerate(lr_range):
         print('.', end='', flush=True)
-        for j, n_epoch in enumerate(ep_range):
+        
+        if L:
+            optimiser = opt(lr, momentum=L)
+        else:
+            optimiser = opt(lr)
+
+        if schedule:
+            optimiser = op.LrScheduleOptimiser(schedule(lr, t), optimiser)
+            
+        model = NN.Model(shapes, afs, optimiser, lamda=Lmd)
+        epochs = 0
+            
+        for j, n_epochs in enumerate(ep_range):
             print(',', end='', flush=True)
-            if L:
-                optimiser = opt(lr, momentum=L)
-            else:
-                optimiser = opt(lr)
 
-            # print(afs)
-            if schedule:
-                model = NN.Model(shapes, afs, op.LrScheduleOptimiser(schedule(lr, t), optimiser), lamda=Lmd)
-            else:
-                model = NN.Model(shapes, afs, optimiser, lamda=Lmd)
-            inputs = data
-
-            Acc, Cross_Ent = lr_ep_error(n_epoch, nr_batches, inputs, targets, test_data, test_targets, costFunc, model)
+            while epochs <= n_epochs:
+                for _ in range(nr_batches):
+                    rand_n = np.random.choice(range(nr_batches))
+                    model.back_propagate(batches[rand_n], batches_targets[rand_n], costFunc)
+                epochs += 1
+            predictions = model.feed_forward(test_data)
+            Acc = Accuracy(predictions.T, test_targets)[0].mean()
+            Cross_Ent = Cross_Entropy(predictions, test_targets)[0].mean()
 
             lr_ep_Cross_ent[k+1, j+1] = Cross_Ent
             lr_ep_Acc[k+1, j+1] = Acc
@@ -101,8 +108,6 @@ def FixedLambda(L, lr_range, ep_range, nr_batches, data, targets, test_data, tes
                 acc_entropy = Cross_Ent
                 k_acc_min = k
                 j_acc_min = j
-
-            # print(Cross_Ent)
             
             if Cross_Ent<min:
                 min = Cross_Ent
@@ -111,7 +116,6 @@ def FixedLambda(L, lr_range, ep_range, nr_batches, data, targets, test_data, tes
                 j_min = j
 
     print('\n')
-    #k = lr, j = epochs
     return [k_min, j_min, min, Cross_acc, k_acc_min, j_acc_min, max, acc_entropy, lr_ep_Acc, lr_ep_Cross_ent]
 
 def FixedLrEpoch(L, lr, ep, nr_batches, data, targets, test_data, test_targets, costFunc, shapes, afs, opt, Lmd_range, schedule, t):
@@ -166,28 +170,35 @@ def Run(L, lr_range, ep_range, nr_batches, data, targets, test_data, test_target
         np.save(f'./Data/NrHidden{len(shapes)-2}/{opt.__name__}/LrEpoch/Acc_{name}', Acc_Image)
         np.save(f'./Data/NrHidden{len(shapes)-2}/{opt.__name__}/LrEpoch/Ent_{name}', Ent_Image)
 
-def RunLambda(L, lr, ep, nr_batches, data, targets, test_data, test_targets, costFunc, shapes, afs, opt, name, Lmd_range, t, schedule):
+def RunLambda(L, lr, ep, nr_batches, data, targets, test_data, test_targets, costFunc, shapes, afs, opt, Lmd_range, t, schedule):
     RL = FixedLrEpoch(L, lr, ep, nr_batches, data, targets, test_data, test_targets, costFunc, shapes, afs, opt, Lmd_range, schedule, t)
     Acc_Ent_Image = RL[-1]
     min = RL[2]
     max = RL[4]
     print(f'Lmd: {Lmd_range[RL[0]]}, Best Acc: {max}, Best CE: {min}')
-    if name:
-        np.makedirs(f'./Data/NrHidden{len(shapes)-2}/{opt.__name__}/Lambda', exist_ok = True)
-        np.save(f'./Data/NrHidden{len(shapes)-2}/{opt.__name__}/Lambda/Acc_Ent', Acc_Ent_Image)
+    os.makedirs(f'./Data/NrHidden{len(shapes)-2}/{opt.__name__}/Lambda', exist_ok = True)
+    np.save(f'./Data/NrHidden{len(shapes)-2}/{opt.__name__}/Lambda/Acc_Ent', Acc_Ent_Image)
 
 def SendToLrEpoch(L, t1, schedule, opt, lr_range, ep_range, Lmd, name):
     shapes = (64, 10)
     train, test, _, train_tar, test_tar, _ = Data.load_data()
-    Run(L, lr_range, ep_range, 22, train, train_tar, test, test_tar, Cross_Entropy, shapes, [AF.SoftMax()], opt, name, Lmd, t1, schedule)
+    Run(L, lr_range, ep_range, 22, train, train_tar, test, test_tar, Cross_Entropy,
+        shapes, [AF.SoftMax()], opt, name, Lmd, t1, schedule)
 
 def SendToLambda(L, Lr, ep, t1, opt, schedule):
     Lmd_range = np.logspace(-5, -3, 50)
     shapes = (64, 10)
     train, test, _, train_tar, test_tar, _ = Data.load_data()
-    name = f't{t1}hyperbolic'
-    RunLambda(L, Lr, ep, 22, train, train_tar, test, test_tar, Cross_Entropy, shapes, [AF.SoftMax()], opt, name, Lmd_range, t1, schedule)
+    RunLambda(L, Lr, ep, 22, train, train_tar, test, test_tar, Cross_Entropy, shapes,
+              [AF.SoftMax()], opt, Lmd_range, t1, schedule)
     
 def NetworkToLrEpoch(L, t1, schedule, opt, lr_range, ep_range, Lmd, name, af, shapes):
     train, test, _, train_tar, test_tar, _ = Data.load_data()
-    Run(L, lr_range, ep_range, 22, train, train_tar, test, test_tar, Cross_Entropy, shapes, [af]*(len(shapes)-2) + [AF.SoftMax()], opt, name, Lmd, t1, schedule)
+    Run(L, lr_range, ep_range, 22, train, train_tar, test, test_tar, Cross_Entropy,
+        shapes, [af]*(len(shapes)-2) + [AF.SoftMax()], opt, name, Lmd, t1, schedule)
+
+def NetworkToLambda(L, Lr, ep, t1, opt, schedule, af, shapes):
+    Lmd_range = np.logspace(-5, -3, 5)
+    train, test, _, train_tar, test_tar, _ = Data.load_data()
+    RunLambda(L, Lr, ep, 22, train, train_tar, test, test_tar, Cross_Entropy, shapes,
+              [af]*(len(shapes)-2)+[AF.SoftMax()], opt, Lmd_range, t1, schedule)
